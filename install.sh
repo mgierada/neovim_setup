066eeb64e135e357ad7d8ab8a48da7d0849a1cb0
Maciej Gierada <maciek.gierada@gmail.com>
2022-08-17 13:39:15 +0200

    updates in install script

--- install.sh
+++ install.sh
@@ -4,5 +4,17 @@
 brew install neovim
 
 # Install NerdFonts
+brew tap homebrew/cask-fonts
 brew install --cask font-hack-nerd-font
-brew install --cask font-hack-nerd-font
+
+# Install vim-pulg
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Bulid coc
+cd $HOME/.local/share/nvim/plugged
+
+yarn install # Copy init.vim file
+
+mkdir -p $HOME/.config/nvim
+cp init.vim $HOME/.config/nvim/.
