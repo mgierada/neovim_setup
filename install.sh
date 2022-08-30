6d13840f9ca66d8c52ea34f71f79dad50e4c9e13
Maciej Gierada <maciek.gierada@gmail.com>
2022-08-30 16:23:41 +0200

    update config

--- install.sh
+++ install.sh
@@ -6,13 +6,14 @@ brew install neovim
 # Install NerdFonts
 brew tap homebrew/cask-fonts
 brew install --cask font-hack-nerd-font
+brew install ctags
 
 # Install vim-pulg
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Bulid coc
-cd $HOME/.local/share/nvim/plugged
+cd $HOME/.local/share/nvim/plugged/coc.vim
 
 yarn install # Copy init.vim file
 
