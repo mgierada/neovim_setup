42c0187717aef03d58f28d90c6b6a74c5437f58d
Maciej Gierada <maciek.gierada@gmail.com>
2022-11-01 22:31:05 +0100

    update__2022_11_01

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -99,6 +99,9 @@ return packer.startup(function(use)
 	-- git integration
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
+	-- github copilot
+	use("github/copilot.vim")
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
