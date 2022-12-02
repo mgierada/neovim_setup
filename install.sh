b191c98269fbda7164ccf9817d4a278e6d44e841
Maciej Gierada <maciek.gierada@gmail.com>
2022-12-02 18:12:58 +0100

    update install script

--- install.sh
+++ install.sh
@@ -8,14 +8,5 @@ brew tap homebrew/cask-fonts
 brew install --cask font-hack-nerd-font
 brew install ctags
 
-# Install vim-pulg
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-# Bulid coc
-cd $HOME/.local/share/nvim/plugged/coc.vim
-
-yarn install # Copy init.vim file
-
 mkdir -p $HOME/.config/nvim
-cp init.vim $HOME/.config/nvim/.
+cp -r lua init.lua $HOME/.config/nvim/.
